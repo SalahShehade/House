aef303177ac600c97774f2669372a31d536fa302
SalahShehada <salah.shehade12@gmail.com>
2024-11-03 12:10:55 +0200

    Task num 8# Ready!

---
+++ Assets/Scripts/Task8# (1) Assignment.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        int Randomnum ;
+        while(i<20){
+            Randomnum = Random.Range(1,21);
+            
+            if(Randomnum == 5){
+                i++;
+                continue;
+            } 
+
+            else if(Randomnum == 15) break;
+
+            Debug.Log(Randomnum);
+            i++;
+
+        }
+          
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
